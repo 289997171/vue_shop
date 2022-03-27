50229342016d520508c855b3a10fe24f5213651b
eclipser1 <5379326+eclipser1@user.noreply.gitee.com>
2022-03-27 20:29:04 +0800

    Login页面添加

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,19 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from "@/views/Login.vue";
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: HomeView
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
+  {
+    path: "/",
+    redirect: '/login' // 重定向
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: ()=> import('../views/Login.vue')
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    登录组件
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from "vue-property-decorator";
+
+@Component({name: "Login"})
+export default class extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
