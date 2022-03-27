3ce2712c40e3e02a074ac8cf598f12145ae2fcd4
eclipser1 <5379326+eclipser1@user.noreply.gitee.com>
2022-03-27 22:06:14 +0800

    登录表单1

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,55 @@
 <template>
   <div class="bg-gray-500 h-full flex justify-center">
     <div class="login_box">
+
+      <!--头像-->
       <div class="flex justify-center">
         <img class="avatar_box" src="@/assets/logo.png" alt="">
       </div>
+
+      {{ loginForm }}
+      <!--表单-->
+      <el-form label-width="100px" class="px-4" :model="loginForm" :rules="loginFromRules" ref="loginFormRef">
+        <el-form-item label="用户名称" prop="username">
+          <el-input prefix-icon="el-icon-search" placeholder="请输入用户名称" v-model="loginForm.username" clearable></el-input>
+        </el-form-item>
+        <el-form-item label="用户密码" prop="password">
+          <el-input prefix-icon="el-icon-search" placeholder="请输入用户密码" v-model="loginForm.password" clearable show-password></el-input>
+        </el-form-item>
+        <el-form-item class="flex justify-end">
+          <el-button type="primary">登录</el-button>
+          <el-button type="reset" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
 
   </div>
 </template>
 
 <script lang="ts">
-import {Vue, Component} from "vue-property-decorator";
+import {Vue, Component, Ref} from "vue-property-decorator";
 
 @Component({name: "Login"})
 export default class extends Vue {
+  @Ref("loginFormRef")
+  private readonly loginFormRef:any;
+  private loginForm = {username: "", password: ""};
 
+  // 表单验证规则
+  private loginFromRules = {
+    username: [
+      {require: true, message: "请输入用户名称", trigger: "blur"},
+      {min: 3, max: 10, message: "长度在 3 到 10个字符", trigger: "blur"}
+    ],
+    password: [
+      {require: true, message: "请输入等领域密码", trigger: "blur"},
+      {min: 6, max: 32, message: "长度在 6 到 32个字符", trigger: "blur"}
+    ]
+  };
+
+  private resetLoginForm() {
+    this.loginFormRef.resetFields();
+  }
 }
 </script>
 
@@ -22,13 +57,13 @@ export default class extends Vue {
 .login_box {
   width: 500px;
   height: 360px;
-  @apply bg-white my-auto rounded;
+@apply bg-white my-auto rounded;
 
   .avatar_box {
     width: 120px;
     height: 120px;
     transform: translateY(-80px);
-    @apply bg-gray-50 rounded-full border-2 shadow-xl;
+  @apply bg-gray-50 rounded-full border-2 shadow-xl;
   }
 }
 </style>
