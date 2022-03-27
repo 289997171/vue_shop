6cf779adba432b6c261f50fd27e93f2991406d92
eclipser1 <5379326+eclipser1@user.noreply.gitee.com>
2022-03-27 23:41:07 +0800

    登录退出

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     Home
+    <el-button type="info" @click="logout">退出</el-button>
   </div>
 </template>
 
@@ -10,6 +11,10 @@ import {Vue, Component} from "vue-property-decorator";
 @Component({name: "Home"})
 export default class extends Vue {
 
+  private logout() {
+    window.sessionStorage.clear();
+    this.$router.push("/login");
+  }
 }
 </script>
 
