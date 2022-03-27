3f1b358f00156f61556ed7ca3a4fd9a07324de4e
eclipser1 <5379326+eclipser1@user.noreply.gitee.com>
2022-03-27 23:38:39 +0800

    导航卫士:跳转拦截器

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import VueRouter, {NavigationGuardNext, Route, RouteConfig} from 'vue-router'
 
 Vue.use(VueRouter)
 
@@ -24,6 +24,17 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+// 导航卫士:跳转拦截器
+router.beforeEach((to:Route, // 将要访问的路径
+                   from:Route, // 来自那个路径
+                   next:NavigationGuardNext) /*跳转工具*/=> {
+  // 如果访问的是登录页面直接泛型
+  if (to.path === "/login") return next();
+  const token = window.sessionStorage.getItem("token");
+  if (token) return next();
+  else return next("/login")
 })
 
 export default router
