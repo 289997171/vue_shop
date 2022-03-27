596726b1f67b8a2f5e427628856603f9c56329df
eclipser1 <5379326+eclipser1@user.noreply.gitee.com>
2022-03-27 22:30:19 +0800

    配置axios

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios';
 import './plugins/element.js'
 
 import '@/assets/css/global.css'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios;
+Vue.prototype.$http.defaults.url = "http://192.168.3.35:8888/api/private/v1/";
 
 new Vue({
   router,
